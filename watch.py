d1f4fff8165ecc11408deebd0130499ed9cb10f0
Ade <kakao1@localhost.localdomain>
2015-08-27 21:29:20 +0900

    fixed for linux

--- watch.py
+++ watch.py
@@ -1,20 +1,27 @@
 import os
 from os.path import getmtime
 import optparse
+import subprocess
+import sys
 
 def watch():
-	'Gets save time of script'
-	parser = optparse.OptionParser()
-	parser.add_option('-f', '--file')
-	options, args = parser.parse_args()
-	watch_file = options.file	
-	last_update_time = getmtime(watch_file)	
-	
-	while True:
-		'Check for change in save time, update save time and run script'
-		if getmtime(watch_file) > last_update_time:
-			last_update_time = getmtime(watch_file)
-			os.system(watch_file)
+    'Gets save time of script'
+    parser = optparse.OptionParser()
+    parser.add_option('-f', '--file')
+    options, args = parser.parse_args()
+    watch_file = options.file
+    last_update_time = getmtime(watch_file)
+    
+    while True:
+    	'Check for change in save time, update save time and run script'
+        try:
+            '''problem on linux where deleting file causes file not found error temporarily.
+             catch exception and continue'''
+            if os.path.exists(watch_file) and  getmtime(watch_file) > last_update_time:
+                last_update_time = getmtime(watch_file)
+                subprocess.call(['python', watch_file])
+        except:
+            pass
 			
 if __name__ == '__main__':    
-	watch()
+    watch()
